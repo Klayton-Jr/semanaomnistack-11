707393a20ae7a379c4a83abefbbf5a7b6f4cbf6e
Kayton-Jr <klaytonjunior@hotmail.com>
2020-03-28 22:39:05 -0300

    Front-end do app e a integração do mesmo com o back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css'
+
+import Routes from './routes';
+
+// JSX (js xml) -- html integrado no js
+
+ // retorna um array com 2 posições -- useState
+  // [valor, função de atualização]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
